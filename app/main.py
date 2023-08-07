c09f29aad71a744f598788b023a9849c1363f643
RNubla <rnubla97@gmail.com>
2023-08-07 11:57:46 -0400

    implemented loadbalancer

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/item")
+def add_item(item: Item):
+    return item
